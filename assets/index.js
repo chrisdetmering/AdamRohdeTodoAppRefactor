90f8fffb8e90e2a1084f2f757d70bae57b2129ab
Adam Rohde <adamrohde@outlook.com>
2020-11-11 15:41:53 -0600

    remove div and remove buttons

--- assets/index.js
+++ assets/index.js
@@ -4,41 +4,57 @@ let i = 1;
 var itemCount;
 
 function addItem(){
-
     itemCount = i++;
-
-    console.log(itemCount);
-
+    //console.log(itemCount);
     var todotext = document.getElementById('todoitem-input').value;
 
     if (todotext.length <= 1){
-
         alert("Please gives yourself something to do!");
-
     }else if (todotext.length > 1){
-
-        console.log("button clicked");
-
+        //console.log("button clicked");
+        //Create div
         var div_element = document.createElement("div");
         div_element.setAttribute("id", "todoItem_" + itemCount);
+        div_element.setAttribute("onClick", "crossOffItem(" + itemCount + ")");
         div_element.appendChild(document.createTextNode(todotext));
         document.getElementById('todo-item-list').appendChild(div_element);
-    
+        //Create button
         var button_element = document.createElement("button");
         button_element.setAttribute("id", "button_" + itemCount);
         button_element.setAttribute("onClick", "removeItem(" + itemCount + ")");
-        button_element.appendChild(document.createTextNode("Complete"));
+        button_element.appendChild(document.createTextNode("Remove"));
         document.getElementById('todo-item-button').appendChild(button_element);
-       
+        document.getElementById('todoitem-input').value = " ";
     }
-
 }
 
-
 function removeItem(item){
-
+    var texttoremove = document.getElementById("button_" + item);
+    var buttontoremove = document.getElementById("todoItem_" + item);
     console.log("Button_ " + item + " Clicked");
-
+    texttoremove.remove();
+    buttontoremove.remove();
 };
 
+function crossOffItem(item){
+    var todotext = document.getElementById('todoItem_' + item).innerText;
+    var result = todotext.strike();
+    document.getElementById('todoItem_' + item).innerHTML = result;
+    console.log(result);
+    console.log("todoItem_" + item + " Clicked" + "Value= " + todotext);
+};
 
+//Handles the adding item when enter is pressed instead of the "Add" button
+var input = document.getElementById('todoitem-input');
+input.addEventListener("keyup", function(event) {
+    // Number 13 is the "Enter" key on the keyboard
+    if (event.keyCode === 13) {
+        console.log("enter not ...clicked");
+
+      // Cancel the default action, if needed
+      event.preventDefault();
+    console.log("enter  clicked");
+
+      addItem();
+    }
+  });
